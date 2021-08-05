5497facd7973bf76846a9589d33cc62ba10fd421
mutharasus <mutharasu.s@gmail.com>
2021-08-05 04:10:39 +0000

    Cleaning up v1 to move all v2 code out of here to keep this stable for users.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
+const cron = require('node-cron');
 const app = express();
 const requestify = require('requestify');
 var userVisitCount = 0;
 
+
 app.get('/', (req, res) => {
   
-  
-  var endResponse = '<h1 style="align: center">List of most recent Confirmed $MIA Mining Transactions</h1><table style="border: 1px solid black;"><thead><tr><th style="border: 1px solid black;">Block Height</th><th style="border: 1px solid black;">Tx_id</th><th style="border: 1px solid black;">Status</th><th style="border: 1px solid black;">Function Type</th><th style="border: 1px solid black;"># of Blocks</th><th style="border: 1px solid black;text-align:left;">Mining Amounts</th></tr></thead><tbody>';
+  var endResponse = '<h1 style="align: center">List of most recent Confirmed $MIA Mining Transactions</h1>&nbsp<h4>This report is provided on an AS IS basis. <i>Please use the <a href="https://explorer.stacks.co/?chain=mainnet">Stacks explorer</a> to verify information provided here is accurate before making any decisions about mining.</i></h4><table style="border: 1px solid black;"><thead><tr><th style="border: 1px solid black;">Block Height</th><th style="border: 1px solid black;">Tx_id</th><th style="border: 1px solid black;">Status</th><th style="border: 1px solid black;">Function Type</th><th style="border: 1px solid black;"># of Blocks</th><th style="border: 1px solid black;text-align:left;">Mining Amounts</th></tr></thead><tbody>';
 
   requestify.get('https://stacks-node-api.stacks.co/extended/v1/tx?limit=200&type%5B%5D=contract_call')
   .then(function(response) {
@@ -52,7 +52,6 @@ app.get('/', (req, res) => {
       };
   }
 );
-  
 });
 
 app.listen(3000, () => {
